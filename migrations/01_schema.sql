9a7bd560f1b91abe58383cb2884c9e98fd7be98b
Lucas <cekanlucas@gmail.com>
2020-03-04 20:26:10 +0000

    initial commit, database and tables

---
+++ migrations/01_schema.sql
@@ -0,0 +1,47 @@
+DROP TABLE users;
+DROP TABLE property_reviews;
+DROP TABLE reservations;
+DROP TABLE properties;
+
+CREATE TABLE property_reviews (
+ id SERIAL PRIMARY KEY,
+ guest_id INTEGER,
+ property_id INTEGER,
+ reservation_id INTEGER,
+ rating SMALLINT,
+ message TEXT
+);
+
+CREATE TABLE users (
+ id SERIAL PRIMARY KEY,
+ name varchar(255),
+ email varchar(255),
+ password varchar(255)
+);
+
+CREATE TABLE reservations (
+ id SERIAL PRIMARY KEY,
+ start_date DATE,
+ end_date DATE,
+ property_id INTEGER,
+ guest_id INTEGER
+);
+
+CREATE TABLE properties (
+ id SERIAL PRIMARY KEY,
+ owner_id INTEGER, 
+ title VARCHAR(255),
+ description TEXT,
+ thumbnail_photo_url VARCHAR(255),
+ cover_photo_url VARCHAR(255),
+ cost_per_night INTEGER,
+ parking_spaces INTEGER,
+ number_of_bathrooms INTEGER, 
+ number_of_bedrooms INTEGER, 
+ country VARCHAR(255),
+ street VARCHAR(255),
+ city VARCHAR(255),
+ province VARCHAR(255),
+ post_code VARCHAR(255),
+ active BOOLEAN
+);
