fd2fc2dde741f789e9621cc69461902876191434
Lucas <cekanlucas@gmail.com>
2020-03-05 18:29:27 +0000

    pool has variables, getAllProperties queries database

--- LightBnB_WebApp-master/server/apiRoutes.js
+++ LightBnB_WebApp-master/server/apiRoutes.js
@@ -1,6 +1,7 @@
 module.exports = function(router, database) {
 
   router.get('/properties', (req, res) => {
+    console.log('TESTING', req.query)
     database.getAllProperties(req.query, 20)
     .then(properties => res.send({properties}))
     .catch(e => {
